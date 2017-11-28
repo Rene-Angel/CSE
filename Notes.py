096b8fe5ad5ffe674de8b739267c3af9673a125a
Rene-Angel <31977772+Rene-Angel@users.noreply.github.com>
2017-11-28 10:46:14 -0800

    Added to previous notes
    11.27.17

--- Notes.py
+++ Notes.py
@@ -1,1 +1,31 @@
 # This is working
+print("Hello World")
+
+# Rene-Angel Jaime
+# (This is python 2.7)
+
+print(3 + 5)
+print(5 - 3)
+print(3 * 5)
+print(6 / 2)
+print(3 ** 2)
+
+print # This makes a new line. In python 3.6, it lokks like: print()
+print("See if you can figure this out")
+print(5 % 3)
+
+car_name = "Chris's car"
+car_type = "Tesla"
+car_cylinders = 8
+car_mpg = 9000.1
+
+# Inline printing
+print("I have a car called the %s" % car_name)
+print("I have a car called the %s. It's a %s." % (car_name, car_type))
+
+#Asking for input
+name = input("What is your name? ") #In python 3, it is called input()
+print("Hello %s." % name)
+
+age = input("How old are you? ")
+print("%s?! Wow, you belong in a retirement home!" % age)
