561fee7b90a66b098e86d6d5d2493ed219f4a6f2
Rene-Angel <31977772+Rene-Angel@users.noreply.github.com>
2017-11-29 10:52:13 -0800

    Added Functions
    11.29.17

--- Notes.py
+++ Notes.py
@@ -1,31 +1,82 @@
-# This is working
-print("Hello World")
+# # This is working
+# print("Hello World")
+#
+# # Rene-Angel Jaime
+# # (This is python 2.7)
+#
+# print(3 + 5)
+# print(5 - 3)
+# print(3 * 5)
+# print(6 / 2)
+# print(3 ** 2)
+#
+# print # This makes a new line. In python 3.6, it lokks like: print()
+# print("See if you can figure this out")
+# print(5 % 3)
+#
+# car_name = "Chris's car"
+# car_type = "Tesla"
+# car_cylinders = 8
+# car_mpg = 9000.1
+#
+# # Inline printing
+# print("I have a car called the %s" % car_name)
+# print("I have a car called the %s. It's a %s." % (car_name, car_type))
+#
+# #Asking for input
+# name = input("What is your name? ") #In python 3, it is called input()
+# print("Hello %s." % name)
+#
+# age = input("How old are you? ")
+# print("%s?! Wow, you belong in a retirement home!" % age)
 
-# Rene-Angel Jaime
-# (This is python 2.7)
+# Functions
 
-print(3 + 5)
-print(5 - 3)
-print(3 * 5)
-print(6 / 2)
-print(3 ** 2)
 
-print # This makes a new line. In python 3.6, it lokks like: print()
-print("See if you can figure this out")
-print(5 % 3)
+def print_hw():
+    print("Hello World")
 
-car_name = "Chris's car"
-car_type = "Tesla"
-car_cylinders = 8
-car_mpg = 9000.1
 
-# Inline printing
-print("I have a car called the %s" % car_name)
-print("I have a car called the %s. It's a %s." % (car_name, car_type))
+print_hw()
+print_hw()
+print_hw()
 
-#Asking for input
-name = input("What is your name? ") #In python 3, it is called input()
-print("Hello %s." % name)
 
-age = input("How old are you? ")
-print("%s?! Wow, you belong in a retirement home!" % age)
+def say_hi(name):  # name is parameter
+    print("Hello %s." % name)
+    print("Enjoy your day.")
+
+
+say_hi("John")
+
+
+def print_age(name, age):
+    print("%s is %d years old." % (name, age))
+    age += 1  # age = age + 1
+    print("Next year, they will be %d" % age)
+
+
+print_age("John", 15)
+
+def f(x):
+    return x**3 + 4 * x**2 + 7 * x - 4
+
+
+print(f(3))
+print(f(4))
+print(f(5))
+
+# If statements
+
+
+def grade_calc(percentage):
+    if percentage >= 90:
+        return "A"
+    elif percentage >= 80:
+        return "B"
+    elif percentage >= 70:
+        return "C"
+    elif percentage >= 60:
+        return "D"
+    else:
+        return "F"
