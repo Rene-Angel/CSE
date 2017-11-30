fb73ed4a9ceba354e0658bd6254009d5db3e9bcd
Rene-Angel <31977772+Rene-Angel@users.noreply.github.com>
2017-11-30 10:48:21 -0800

    Added Loops
    11.30.17

--- Notes.py
+++ Notes.py
@@ -1,8 +1,8 @@
-# # This is working
+# This is working
 # print("Hello World")
 #
-# # Rene-Angel Jaime
-# # (This is python 2.7)
+# Rene-Angel Jaime
+# (This is python 2.7)
 #
 # print(3 + 5)
 # print(5 - 3)
@@ -33,50 +33,90 @@
 # Functions
 
 
-def print_hw():
-    print("Hello World")
+# def print_hw():
+#     print("Hello World")
 
 
-print_hw()
-print_hw()
-print_hw()
+# print_hw()
+# print_hw()
+# print_hw()
 
 
-def say_hi(name):  # name is parameter
-    print("Hello %s." % name)
-    print("Enjoy your day.")
+# def say_hi(name):  # name is parameter
+#     print("Hello %s." % name)
+#     print("Enjoy your day.")
 
 
-say_hi("John")
+# say_hi("John")
 
 
-def print_age(name, age):
-    print("%s is %d years old." % (name, age))
-    age += 1  # age = age + 1
-    print("Next year, they will be %d" % age)
+# def print_age(name, age):
+#     print("%s is %d years old." % (name, age))
+#     age += 1  # age = age + 1
+#     print("Next year, they will be %d" % age)
 
 
-print_age("John", 15)
-
-def f(x):
-    return x**3 + 4 * x**2 + 7 * x - 4
+# print_age("John", 15)
 
 
-print(f(3))
-print(f(4))
-print(f(5))
+# def f(x):
+#     return x ** 3 + 4 * x ** 2 + 7 * x - 4
+
+
+# print(f(3))
+# print(f(4))
+# print(f(5))
+
 
 # If statements
 
 
-def grade_calc(percentage):
-    if percentage >= 90:
-        return "A"
-    elif percentage >= 80:
-        return "B"
-    elif percentage >= 70:
-        return "C"
-    elif percentage >= 60:
-        return "D"
-    else:
-        return "F"
+# def grade_calc(percentage):
+#     if percentage >= 90:
+#         return "A"
+#     elif percentage >= 80:
+#         return "B"
+#     elif percentage >= 70:
+#         return "C"
+#     elif percentage >= 60:
+#         return "D"
+#     else:
+#         return "F"
+
+
+# '''Write a function called "happy_bday"
+# that "sings" (prints) Happy birthday
+
+# It must take one parameter called "name"
+# '''
+
+
+# def happy_bday(name):
+#     print("Happy Birthday to you" + ",")
+#     print("Happy Birthday to you" + ",")
+#     print("Happy Birthday to " + name + ",")
+#     print("Happy Birthday to you" + ".")
+
+# happy_bday("John")
+
+
+# Loops
+
+
+# for num in range(1000000):
+#     print(num + 1)
+
+# DO NOT RUN!!!
+# a = 1
+# while True:
+#     print(a)
+
+# a = 1
+# while a < 10:
+#     print(a)
+#     a += 1
+
+# Random numbers
+
+# import random  This should be on line 1
+# print(random.randint(0, 100))
