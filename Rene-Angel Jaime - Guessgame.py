8a60963562c6288dfc64d349df842f723745b1ab
Rene-Angel <31977772+Rene-Angel@users.noreply.github.com>
2017-12-07 10:47:59 -0800

    Added Warmups
    12.7.17

--- Rene-Angel Jaime - Guessgame.py
+++ Rene-Angel Jaime - Guessgame.py
@@ -1,6 +1,6 @@
 # Rene-Angel Jaime
 # 1) Generate random number
-# 2) ake and input (number) form the user
+# 2) Take and input (number) form the user
 # 3) Compare input to generated number
 # 4) Add "higher" and "lower" statements
 # 5) Add 5 guesses
@@ -12,18 +12,18 @@ guessesTaken = 0
 print("Hello! What is your name?")
 myName = input()
 
-number = random.randint(1, 50)
+number = random.randint(1, 50)  # 1) Generate random number
 print("Well %s, I am thinking of a number between 1 and 50." % myName)
 
 guess = ''
-while guessesTaken < 5 and guess != number:
+while guessesTaken < 5 and guess != number:  # 5) Add 5 guesses
     print("Take a guess.")
-    guess = input()
-    guess = int(guess)
+    guess = input()  # 2) Take and input (number) form the user
+    guess = int(guess)  # 3) Compare input to generated number
 
     guessesTaken += 1
 
-    if guess < number:
+    if guess < number:  # 4) Add "higher" and "lower" statements
         print("Your guess is too low.")
     elif guess > number:
         print("Your guess is too high.")
--- Warmups.py
+++ Warmups.py
@@ -12,13 +12,14 @@
 #     print(last_name + " " + first_name)
 
 
-def reverse_order(first_name, last_name):
-    first_name = input("First name")
-    last_name = input("Last name")
-    print("%s, %s" % last_name, first_name)
+# def reverse_order(first_name, last_name):
+#     first_name = input("First name")
+#     last_name = input("Last name")
+#     print("%s, %s" % last_name, first_name)
 
 
 # 12.5.17
+
 
 """
 add_py("I_eat") == "I_eat.py"
@@ -30,14 +31,34 @@ def add_py(name):
 
 
 # 12.6.17
-"""Write a function called"add"
-which takes three parameters
-and prints the sum of the numbers
-"""
+# """Write a function called"add"
+# which takes three parameters
+# and prints the sum of the numbers
+# """
 
 
-def add(num1, num2, num3):
-    print(num1 + num2 + num3)
+# def add(num1, num2, num3):
+#     print(num1 + num2 + num3)
 
 
-add(90, 900, 9000)
+# add(90, 900, 9000)
+
+# 12.7.17
+# Write a function called "repeat"
+# that takes one parameter (string)
+# and prints it three times.
+#
+# example:
+# repeat("Hello") prints:
+# Hello
+# Hello
+# Hello
+
+
+# def repeat(string):
+#     print(string)
+#     print(string)
+#     print(string)
+
+#     for x in range(3):
+#         print(string)
