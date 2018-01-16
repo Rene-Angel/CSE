eb756d45fcd1d6418ab4afe7a8e541e390a263f6
Rene-Angel <31977772+Rene-Angel@users.noreply.github.com>
2018-01-16 10:47:55 -0800

    1.16.18
    added loops

--- Rene-Angel Jaime - Luckysevens.py
+++ Rene-Angel Jaime - Luckysevens.py
@@ -1,15 +1,17 @@
 # Rene-Angel Jaime
 import random
-
 cash = 15
-d1 = (random.randint(1, 6))
-d2 = (random.randint(1, 6))
-dtotal = d1 + d2
-print("Dice:" + "%s" % dtotal)
 
 while cash > 0:
-    cash = cash - 1
-    print(cash)
-if dtotal == 7:
-    cash = cash + 5
-    print("Cash:" + "%s" % cash)
+    d1 = (random.randint(1, 6))
+    d2 = (random.randint(1, 6))
+    total = d1 + d2
+    print("Rolled: %s" % total)
+    print("Cash: %s" % cash)
+
+    if d1 + d2 == 7:
+        cash += 4
+        print(cash)
+    else:
+        cash -= 1
+        print(cash)
