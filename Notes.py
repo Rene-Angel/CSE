524f999c2992cc726fac6352f17a185477b2acaf
Rene-Angel <31977772+Rene-Angel@users.noreply.github.com>
2018-01-18 10:47:26 -0800

    1.18.18
    added notes

--- Notes.py
+++ Notes.py
@@ -122,16 +122,47 @@
 # print(random.randint(0, 100))
 
 # Comparisons
-print(1 == 1)  # Is 1 equal to 1?
-print(1 != 2)  # Is 1 not equal to 2?
-print(10 <= 15)
-print(not False)
+# print(1 == 1)  # Is 1 equal to 1?
+# print(1 != 2)  # Is 1 not equal to 2?
+# print(10 <= 15)
+# print(not False)
 
 # Recasting
 
-c = '1'
-print(c == 1)
-print(int(c) == 1)  # Both are ints
-print(c == str(1))  # Both are strings
+# c = '1'
+# print(c == 1)
+# print(int(c) == 1)  # Both are ints
+# print(c == str(1))  # Both are strings
 
 # The input command ALWAYS gives a string
+
+# Lists
+the_count = {1, 2, 3, 4, 5}
+shopping_list = ["Noodles", "Eggrolls", "Milk", "rice", "soda", "chips"]
+
+print(shopping_list[0])
+print(shopping_list[2])
+
+print(len(shopping_list))
+
+# Going through a list
+for item in shopping_list:
+    print(item)
+
+for num in the_count:
+    print(num * 2)
+
+len(shopping_list)  # Gives me the length of the list
+range(3)  # Gives a list of the numbers 0 through 2
+range(len(shopping_list))  # A list of EVERY index in a list
+
+for num in range(len(shopping_list)):
+    item = shopping_list[num]
+    print("The item at index %d is %s" % (num, item))
+
+# Turn things into a list
+str1 = "Hello Class!"
+listOne = list(str1)
+print(listOne)
+listOne[11] = '.'
+print(listOne)
