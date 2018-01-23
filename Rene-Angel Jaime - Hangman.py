be9f61b4d55e6bc8caf9516d0e9dfd352551b6ca
Rene-Angel <31977772+Rene-Angel@users.noreply.github.com>
2018-01-23 10:47:00 -0800

    1.23.18
    added to Hangman

--- Rene-Angel Jaime - Hangman.py
+++ Rene-Angel Jaime - Hangman.py
@@ -1,16 +1,18 @@
+import random
 import string
 """
 Rene-Angel Jaime
 A general guide for Hangman
 1. Make a word bank - 10 items
 2. Pick a random item from the list
-3. Hide teh word(use *)
+3. Add a guess to the list of letters guessed
 4. Reveal letters already guessed
 5. Create the win condition
 """
-
+guesses = 10
 word_bank = ["Batman", "Harley Quinn", "Supergirl", "Black Adam", "Atrocitus", "Aquaman", "Blue Beetle",
              "Wonder Woman", "Black Canary", "Green Lantern"]
-# random.shuffle(word_bank)
-for item in word_bank:
-    print(item)
+
+str1 = (random.choice(word_bank))
+listOne = list(str1)
+print(listOne)
