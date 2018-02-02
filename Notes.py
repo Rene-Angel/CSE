2832498d4ab9e3e177ca7f6cfe0ddf5150e50251
Rene-Angel <31977772+Rene-Angel@users.noreply.github.com>
2018-02-02 11:14:59 -0800

    2.2.18
    Added Notes

--- Notes.py
+++ Notes.py
@@ -137,57 +137,93 @@
 # The input command ALWAYS gives a string
 
 # Lists
-the_count = {1, 2, 3, 4, 5}
-shopping_list = ["Noodles", "Eggrolls", "Milk", "rice", "soda", "chips"]
+# the_count = {1, 2, 3, 4, 5}
+# shopping_list = ["Noodles", "Eggrolls", "Milk", "rice", "soda", "chips"]
 
-print(shopping_list[0])
-print(shopping_list[2])
-
-print(len(shopping_list))
+# print(shopping_list[0])
+# print(shopping_list[2])
+#
+# print(len(shopping_list))
 
 # Going through a list
-for item in shopping_list:
-    print(item)
-
-for num in the_count:
-    print(num * 2)
-
-len(shopping_list)  # Gives me the length of the list
-range(3)  # Gives a list of the numbers 0 through 2
-range(len(shopping_list))  # A list of EVERY index in a list
-
-for num in range(len(shopping_list)):
-    item = shopping_list[num]
-    print("The item at index %d is %s" % (num, item))
+# for item in shopping_list:
+#     print(item)
+#
+# for num in the_count:
+#     print(num * 2)
+#
+# len(shopping_list)  # Gives me the length of the list
+# range(3)  # Gives a list of the numbers 0 through 2
+# range(len(shopping_list))  # A list of EVERY index in a list
+#
+# for num in range(len(shopping_list)):
+#     item = shopping_list[num]
+#     print("The item at index %d is %s" % (num, item))
 
 # Turn things into a list
-str1 = "Hello Class!"
-listOne = list(str1)
-print(listOne)
-listOne[11] = '.'
-print(listOne)
-print("".join(listOne))
+# str1 = "Hello Class!"
+# listOne = list(str1)
+# print(listOne)
+# listOne[11] = '.'
+# print(listOne)
+# print("".join(listOne))
 
 # Adding things from a list
-shopping_list.append("cereal")
-shopping_list.append("salad")
-print(shopping_list)
+# shopping_list.append("cereal")
+# shopping_list.append("salad")
+# print(shopping_list)
 
 # Removing things from a list
-shopping_list.remove("soda")
-print(shopping_list)
-shopping_list.pop(0)
-print(shopping_list)
+# shopping_list.remove("soda")
+# print(shopping_list)
+# shopping_list.pop(0)
+# print(shopping_list)
 
 # The string class
-import string
-print(string.ascii_letters)
-print(string.ascii_lowercase)
-print(string.ascii_uppercase)
-print(string.punctuation)
-print(string.digits)
+# import string
+# print(string.ascii_letters)
+# print(string.ascii_lowercase)
+# print(string.ascii_uppercase)
+# print(string.punctuation)
+# print(string.digits)
 
 # Dealing with strings
-strTwo = "ThIs iS a VeRY oDd sEnTenCE"
-lowercase = strTwo.lower()
-print(lowercase)
+# strTwo = "ThIs iS a VeRY oDd sEnTenCE"
+# lowercase = strTwo.lower()
+# print(lowercase)
+
+# Dictionaries - Made up of Key: Value Pairs
+dictionary = {'name': 'Lance', 'age': 23, "height": 5 * 12 + 7}
+
+# Accessing dictionaries
+print(dictionary['name'])
+print(dictionary['age'])
+print(dictionary['height'])
+
+large_dictionary = {
+    'CA': 'California',
+    'FL': 'Florida',
+    'OH': 'Ohio'
+}
+
+print(large_dictionary['FL'])
+
+larger_dictionary = {
+    'CA': [
+        'Fresno',
+        'Sacramento',
+        'Los Angeles'
+    ],
+    'FL': [
+        'Tampa',
+        'Orlando',
+        'Miami'
+    ],
+    'OH': [
+        'Cleavland',
+        'Cincinnati'
+    ]
+}
+print(larger_dictionary['FL'])
+print(larger_dictionary['FL'][2])
+print(larger_dictionary['OH'][1])
