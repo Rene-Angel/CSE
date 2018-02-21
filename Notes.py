9c5edb13a2fb4aba4bbd06af22104fc468dea084
Rene-Angel <31977772+Rene-Angel@users.noreply.github.com>
2018-02-21 10:47:04 -0800

    2.21.18
    Added Class Notes

--- Notes.py
+++ Notes.py
@@ -192,80 +192,80 @@
 # lowercase = strTwo.lower()
 # print(lowercase)
 
-# Dictionaries - Made up of Key: Value Pairs
-dictionary = {'name': 'Lance', 'age': 23, "height": 5 * 12 + 7}
-
-# Accessing dictionaries
-print(dictionary['name'])
-print(dictionary['age'])
-print(dictionary['height'])
-
-# Adding to a Dictionary
-dictionary['weight'] = 280
-print(dictionary)
-
-large_dictionary = {
-    'CA': 'California',
-    'FL': 'Florida',
-    'OH': 'Ohio'
-}
-
-print(large_dictionary['FL'])
-
-larger_dictionary = {
-    'CA': [
-        'Fresno',
-        'Sacramento',
-        'Los Angeles'
-    ],
-    'FL': [
-        'Tampa',
-        'Orlando',
-        'Miami'
-    ],
-    'OH': [
-        'Cleavland',
-        'Cincinnati'
-    ]
-}
-print(larger_dictionary['FL'])
-print(larger_dictionary['FL'][2])
-print(larger_dictionary['OH'][1])
-
-largest_dictionary = {
-    'CA': {
-        'NAME': 'California',
-        'POPULATION': 39250000,
-        'BORDER ST': [
-            'Oregon',
-            'Nevada',
-            'Arizona'
-        ]
-    },
-    'AZ': {
-        'NAME': 'Arizona',
-        'POPULATION': 6931000,
-        'BORDER ST': [
-            'California',
-            'Utah',
-            'Nevada',
-            'New Mexico'
-        ]
-    },
-    'NY': {
-        'NAME': "New York",
-        'POPULATION': 19750000,
-        'BORDER ST': [
-            'Vermont',
-            'Massachusetts',
-            'Connecticut',
-            'Pennsylvania',
-            'New Jersey'
-        ]
-    }
-}
-
-current_node = largest_dictionary['CA']
-print(current_node)
-print(current_node['NAME'])
-print(current_node['POPULATION'])
+# # Dictionaries - Made up of Key: Value Pairs
+# dictionary = {'name': 'Lance', 'age': 23, "height": 5 * 12 + 7}
+#
+# # Accessing dictionaries
+# print(dictionary['name'])
+# print(dictionary['age'])
+# print(dictionary['height'])
+#
+# # Adding to a Dictionary
+# dictionary['weight'] = 280
+# print(dictionary)
+#
+# large_dictionary = {
+#     'CA': 'California',
+#     'FL': 'Florida',
+#     'OH': 'Ohio'
+# }
+#
+# print(large_dictionary['FL'])
+#
+# larger_dictionary = {
+#     'CA': [
+#         'Fresno',
+#         'Sacramento',
+#         'Los Angeles'
+#     ],
+#     'FL': [
+#         'Tampa',
+#         'Orlando',
+#         'Miami'
+#     ],
+#     'OH': [
+#         'Cleavland',
+#         'Cincinnati'
+#     ]
+# }
+# print(larger_dictionary['FL'])
+# print(larger_dictionary['FL'][2])
+# print(larger_dictionary['OH'][1])
+#
+# largest_dictionary = {
+#     'CA': {
+#         'NAME': 'California',
+#         'POPULATION': 39250000,
+#         'BORDER ST': [
+#             'Oregon',
+#             'Nevada',
+#             'Arizona'
+#         ]
+#     },
+#     'AZ': {
+#         'NAME': 'Arizona',
+#         'POPULATION': 6931000,
+#         'BORDER ST': [
+#             'California',
+#             'Utah',
+#             'Nevada',
+#             'New Mexico'
+#         ]
+#     },
+#     'NY': {
+#         'NAME': "New York",
+#         'POPULATION': 19750000,
+#         'BORDER ST': [
+#             'Vermont',
+#             'Massachusetts',
+#             'Connecticut',
+#             'Pennsylvania',
+#             'New Jersey'
+#         ]
+#     }
+# }
+#
+# current_node = largest_dictionary['CA']
+# print(current_node)
+# print(current_node['NAME'])
+# print(current_node['POPULATION'])
