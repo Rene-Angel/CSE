60244444b7e989555d6ab86c22c5ecc65fdd2a94
Rene-Angel <31977772+Rene-Angel@users.noreply.github.com>
2018-03-08 10:46:46 -0800

    3.8.18
    Added Character Methods

--- Rene-Angel Jaime - Characters.py
+++ Rene-Angel Jaime - Characters.py
@@ -1,26 +1,38 @@
 class Character(object):
-    def __init__(self, name, health, status, height, weight, race, description, inventory):
+    def __init__(self, name, health, attack, status_effects, information, inventory):
         self.name = name
         self.health = health
-        self.status = status
-        self.physique = height, weight, race
-        self.description = description
+        self.status_effects = status_effects
+        self.information = information
         self.inventory = inventory
+        self.attack_amt = attack
+        self.talk = "Hello."
+
+    def attack(self, target):
+        if target.damage(self.attack_amt):
+
+    def take_damage(self, amt):
+
+    def interact(self):
+        item.inventory
+
+    def take_damage(self):
 
 
-first_person = Character("Rodin", 10000, "ally", "6'5 ft", "230.5 lbs", "Archangel",
-                         "'He comes from the heavens above, be as one of the old archangels he wants nothing more than"
-                         " to destroy those who have defied him.' He's tall, dark, and of course handsome. He is "
-                         "nothing more than a weapon's blacksmith with an agenda to run his bar, 'Hell's Gate'. His "
-                         "biggest client is a witch who knows her thing or two about hunting angels and she brings all "
-                         "the goods that Rodin needs, while Rodin himself pours her a drink and starts creating her "
-                         "weapons out of the Halos, Demon Blood, Ebony, and Angelic Tunes from the Heavens. He is "
-                         "very picky when it comes to getting the job done!",
-                         "Inventory: Angel's Sword, Health Vials, Bar Key")
+rodin = Character("Rodin", 10000, 99, "Immortality",
+                  "'He comes from the heavens above, be as one of the old archangels he wants nothing more than "
+                  "to destroy those who have defied him.' He's tall, dark, and powerful foe. He is "
+                  "nothing more than a weapon's blacksmith with an agenda to run his bar, 'Hell's Gate'. His "
+                  "biggest client is a witch who knows her thing or two about hunting angels and she brings all "
+                  "the goods that Rodin needs, while Rodin himself pours her a drink and starts creating her "
+                  "weapons out of the Halos, Demon Blood, Ebony, and Angelic Tunes from the Heavens. He is "
+                  "very picky when it comes to getting the job done!",
+                  "Angelic Sword, Demon's Eye, Health Vials, Bar Keys")
 
-print(first_person.name)
-print(first_person.health)
-print(first_person.status)
-print(first_person.physique)
-print(first_person.description)
-print(first_person.inventory)
+print("Name: %s" % rodin.name)
+print("Health: %s" % rodin.health)
+print("Attack Power: %s" % rodin.attack)
+print("Status Effect(s): %s" % rodin.status_effects)
+print(rodin.physique)
+print(rodin.information)
+print("Inventory: %s" % rodin.inventory)
