1aeaaab47c3555e3caed7c87bb35e77aa85f075d
Rene-Angel <31977772+Rene-Angel@users.noreply.github.com>
2018-03-21 10:46:54 -0700

    3.21.18
    Added More Items

--- Rene-Angel Jaime - Items.py
+++ Rene-Angel Jaime - Items.py
@@ -3,31 +3,47 @@ class Item(object):
         self.name = name
         self.info = info
         self.value = value
-        self.use = True
+        self.take = True
+        self.drop = False
 
     def drop(self):
-        self.drop()
+        self.take = False
+        self.drop = True
+        print("You take the item.")
+
+    def take(self):
+        self.take = True
+        self.drop = False
+        print("You take the item.")
 
     def sell(self):
         self.value += 100
 
 
 class Weapon(Item):
-    def __init__(self, name, info, attack, durability):
-        super(Weapon, self).__init__(name, info, range)
-        self.attack = attack
+    def __init__(self, name, info, durability, value):
+        super(Weapon, self).__init__(name, info, value)
+        self.attack = False
         self.durability = durability
-        self.range = range
         self.target = ()
 
     def damage(self, target):
         self.target = ()
-        self.attack = ()
+        self.attack = True
         target.hp -= 100
 
 
 class Consumable(Item):
-    def __init__(self, name, info, use, effect):
-        super(Consumable, self).__init__(name, info, effect)
-        self.use = use
+    def __init__(self, name, info, effect, value):
+        super(Consumable, self).__init__(name, info, value)
+        self.use = False
         self.effect = effect
+
+    def use(self):
+        self.use = True
+        print("You used this item")
+        self.drop = True
+
+
+bone_sword = Weapon("Bone Sword", "Created by the skeletal remains of fallen warriors.", 100, 25)
+hp_potion = Consumable("Health Potion", "Use for injuries", "Healing", 75)
