5ad91f4e889981f2a9e9df0dba7c13531782ebb0
Rene-Angel <31977772+Rene-Angel@users.noreply.github.com>
2018-03-22 10:46:30 -0700

    3.22.18
    Added More Items

--- Rene-Angel Jaime - Items.py
+++ Rene-Angel Jaime - Items.py
@@ -1,10 +1,11 @@
 class Item(object):
-    def __init__(self, name, info, value):
+    def __init__(self, name, value):
         self.name = name
-        self.info = info
         self.value = value
         self.take = True
         self.drop = False
+        self.health = 1000
+        self.target_health = 1000
 
     def drop(self):
         self.take = False
@@ -21,29 +22,94 @@ class Item(object):
 
 
 class Weapon(Item):
-    def __init__(self, name, info, durability, value):
-        super(Weapon, self).__init__(name, info, value)
-        self.attack = False
-        self.durability = durability
-        self.target = ()
+    def __init__(self, name, attack, value):
+        super(Weapon, self).__init__(name, value)
+        self.attack = attack
 
-    def damage(self, target):
-        self.target = ()
+    def attack(self):
         self.attack = True
-        target.hp -= 100
+        self.target_health -= self.attack
+
+
+class Ammunition(Item):
+    def __init__(self, name, damage, amount):
+        super(Ammunition, self).__init__(name, value)
+        self.damage = damage
+        self.amount = amount
+
+    def fire(self):
+        self.target_health -= self.damage
 
 
 class Consumable(Item):
-    def __init__(self, name, info, effect, value):
-        super(Consumable, self).__init__(name, info, value)
+    def __init__(self, name, effect, value):
+        super(Consumable, self).__init__(name, value)
         self.use = False
         self.effect = effect
 
     def use(self):
         self.use = True
         print("You used this item")
+        self.health += self.effect
+        self.effect = True
         self.drop = True
 
 
-bone_sword = Weapon("Bone Sword", "Created by the skeletal remains of fallen warriors.", 100, 25)
-hp_potion = Consumable("Health Potion", "Use for injuries", "Healing", 75)
+class Bone_Sword(Weapon):
+    def __init__(self):
+        super(Bone_Sword, self).__init__("Bone Sword", 5, 10)
+
+
+class War_Axe(Weapon):
+    def __init__(self):
+        super(War_Axe, self).__init__("War Axe", 15, 20)
+
+
+class X_Bow(Weapon):
+    def __init__(self):
+        super(X_Bow, self).__init__("Cross Bow", 20, 40)
+
+
+class Arrows(Ammunition):
+    def __init__(self):
+        super(Arrows, self).__init__("Arrows", 25, 50)
+
+
+class Health_Potion(Consumable):
+    def __init__(self):
+        super(Health_Potion, self).__init__("Health Potion", 100, 100)
+
+
+class Apple(Consumable):
+    def __init__(self):
+        super(Apple, self).__init__("Apple", 5, 1)
+
+
+class Branch(Weapon):
+    def __init__(self):
+        super(Branch, self).__init__("Tree Branch", 10, 1)
+
+
+class Dagger(Weapon):
+    def __init__(self):
+        super(Dagger, self).__init__("Dagger", 15, 10)
+
+
+class Egg(Consumable):
+    def __init__(self):
+        super(Egg, self).__init__("Egg", 10, 5)
+
+
+class Long_Bow(Ammunition):
+    def __init__(self):
+        super(Long_Bow, self).__init__("Long Bow", 20, 30)
+
+
+class Stick(Item):
+    def __init__(self):
+        super(Stick, self).__init__("Stick", 0)
+
+    def use(self):
+        print("It's just a stick what more do you want?")
+
+
