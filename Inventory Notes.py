e82cf0ff4c59b7b77e43feef0ac21e05c411956d
Rene-Angel <31977772+Rene-Angel@users.noreply.github.com>
2018-04-30 11:47:19 -0700

    4.30.18
    Added Inventory Notes and List

---
+++ Inventory Notes.py
@@ -0,0 +1,38 @@
+import string
+
+
+class Item(object):
+    def __init__(self, name, attack, armor, weight, price):
+        self.name = name
+        self.attack = attack
+        self.armor = armor
+        self.weight = weight
+        self.price = price
+
+
+class Inventory(object):
+    def __init__(self):
+        self.items = {}  # Creates the List to place in 'items'
+
+    def take(self, item):  # By creating the take method, it can add in an item to the inventory
+        self.items[item.name] = item
+    #
+    # def drop(self, item):
+    #     inventory.remove[item]
+
+    def print_inventory(self):  # Adds or 'joins' in other callable categories for balance?
+        # print('\t'.join(['Name', 'Atk', 'Arm', 'Lb', 'Val'])) # This would add in other values to be printed
+        for item in self.items.values():  # With the current changes, it only prints the name :)
+            print('\t'.join([str(x) for x in [item.name]]))  # , item.attack, item.armor, item.weight, item.price]]))
+        # for item in self.items():  # Lines of Code aren't callable and creates a TypeError
+            # print(item.name)
+
+
+Sword = Item('Sword', 12, 0, 10, 20)  # By creating the object outside of the inventory, it can be added
+inventory = Inventory()
+inventory.take(Sword)  # Adding this would call an item and add it into the inventory list
+# inventory.take(Item('Sword', 15, 0, 12, 20))  # Adding this would create the object within the inventory
+inventory.print_inventory()
+
+inv = list(inventory)
+while True:
--- Rene-Angel Jaime - CYOA.py
+++ Rene-Angel Jaime - CYOA.py
@@ -16,14 +16,6 @@ class Item(object):
         self.weight = weight
         self.price = price
 
-    def discard(self):
-        print("You dropped %s." % self.name)
-
-    def obtain(self):
-        print("You obtained %s." % self.name)
-
-    # def sell(self):
-
 
 class Weapon(Item):
     def __init__(self):
@@ -130,156 +122,150 @@ class Room(object):
         self.east = east
         self.west = west
         self.description = description
-        self.items = items
+        self.items = items()
         self.character = character
 
     def move(self, direction):
         global current_node
         current_node = globals()[getattr(self, direction)]
 
-    def take(self, items):
-        global current_item
-        current_item = globals()[getattr(self, items)]
-
 
 hallway_1 = Room("Long Corridor", 'hallway_2', 'hallway_3', 'wall_opening', None,
-                 {ragnarok, war_axe}, None, "This long Hallway leads to: "
-                 "North: Long Corridor "
-                 "South: Long Corridor "
-                 "East: Broken Wall Opening ")
+                 Item[ragnarok, war_axe], None, 'This long Hallway leads to: '
+                                                'North: Long Corridor '
+                                                'South: Long Corridor '
+                                                'East: Broken Wall Opening ')
 hallway_2 = Room("Long Corridor", 'hallway_6', 'hallway_1', None, 'broom_closet',
-                 {apple, sword}, None, "This Long Hallway leads to: "
-                 "North: Long Corridor "
-                 "South: Long Corridor "
-                 "East: "
-                 "West: ")
+                 Item[apple, sword], None, 'This Long Hallway leads to: '
+                                           'North: Long Corridor '
+                                           'South: Long Corridor '
+                                           'East: '
+                                           'West: ')
 hallway_3 = Room("Long Corridor", 'hallway_1', None, 'hallway_4', None,
-                 {None}, None, "This Long Hallway leads to: "
-                 "North: Long Corridor "
-                 "South: "
-                 "East: Long Corridor "
-                 "West: ")
+                 None, None, 'This Long Hallway leads to: '
+                             'North: Long Corridor '
+                             'South: '
+                             'East: Long Corridor '
+                             'West: ')
 hallway_4 = Room("Long Corridor", None, 'hallway_5', None, 'hallway_3',
-                 {None}, None, "This Long Hallway leads to: "
-                 "North: Long Corridor "
-                 "South: "
-                 "East: "
-                 "West: Long Corridor ")
+                 None, None, 'This Long Hallway leads to: '
+                             'North: Long Corridor '
+                             'South: '
+                             'East: '
+                             'West: Long Corridor ')
 hallway_5 = Room("Long Corridor", 'hallway_4', 'hallway_6', None, None,
-                 {None}, None, "This Long Hallway leads to: "
-                 "North: Long Corridor "
-                 "South: Long Corridor "
-                 "East: "
-                 "West: ")
+                 None, None, 'This Long Hallway leads to: '
+                             'North: Long Corridor '
+                             'South: Long Corridor '
+                             'East: '
+                             'West: ')
 hallway_6 = Room("Long Corridor", None, 'hallway_2', None, 'hallway_7',
-                 {None}, None, "This Long Hallway leads to: "
-                 "North: "
-                 "South: Long Corridor "
-                 "East: "
-                 "West: Long Corridor ")
+                 None, None, 'This Long Hallway leads to: '
+                             'North: '
+                             'South: Long Corridor '
+                             'East: '
+                             'West: Long Corridor ')
 hallway_7 = Room("Long Corridor", None, None, 'hallway_6', 'hallway_8',
-                 {None}, None, "This Long Hallway leads to: "
-                 "North: "
-                 "East: Long Corridor "
-                 "West: Long Corridor ")
+                 None, None, 'This Long Hallway leads to: '
+                             'North: '
+                             'East: Long Corridor '
+                             'West: Long Corridor ')
 hallway_8 = Room("Long Corridor", None, None, 'hallway_7', 'hallway_9',
-                 {None}, None, "This Long Hallway leads to: "
-                 "North: "
-                 "East: Long Corridor "
-                 "West: Long Corridor ")
+                 None, None, 'This Long Hallway leads to: '
+                             'North: '
+                             'East: Long Corridor '
+                             'West: Long Corridor ')
 hallway_9 = Room("Long Corridor", None, None, 'hallway_8', 'hallway_10',
-                 {None}, None, "This Long Hallway leads to: "
-                 "North: "
-                 "East: Long Corridor "
-                 "West: Long Corridor ")
+                 None, None, 'This Long Hallway leads to: '
+                             'North: '
+                             'East: Long Corridor '
+                             'West: Long Corridor ')
 hallway_10 = Room("Long Corridor", None, None, 'hallway_9', None,
-                  {None}, None, "This Long Hallway leads to: "
-                  "North: "
-                  "East: Long Corridor "
-                  "West: ")
+                  None, None, 'This Long Hallway leads to: '
+                              'North: '
+                              'East: Long Corridor '
+                              'West: ')
 hallway_11 = Room("Long Corridor", None, None, None, None,
-                  {None}, None, "This Long Hallway leads to: "
-                  "North: "
-                  "South: "
-                  "East: ")
+                  None, None, 'This Long Hallway leads to: '
+                              'North: '
+                              'South: '
+                              'East: ')
 hallway_12 = Room("Long Corridor", None, None, 'hallway_24', 'hallway_13',
-                  {None}, None, "This Long Hallway leads to: "
-                  "South: "
-                  "East: Long Corridor "
-                  "West: Long Corridor ")
+                  None, None, 'This Long Hallway leads to: '
+                              'South: '
+                              'East: Long Corridor '
+                              'West: Long Corridor ')
 hallway_13 = Room("Long Corridor", 'hallway_14', None, 'hallway_18', 'hallway_12',
-                  {None}, None, "This Long Hallway leads to: "
-                  "North: Long Corridor "
-                  "East: Long Corridor "
-                  "West: Long Corridor ")
+                  None, None, 'This Long Hallway leads to: '
+                              'North: Long Corridor '
+                              'East: Long Corridor '
+                              'West: Long Corridor ')
 hallway_14 = Room("Long Corridor", 'hallway_15', 'hallway_13', None, None,
-                  {None}, None, "This Long Hallway leads to: "
-                  "North: Long Corridor "
-                  "South: Long Corridor "
-                  "East: ")
+                  None, None, 'This Long Hallway leads to: '
+                              'North: Long Corridor '
+                              'South: Long Corridor '
+                              'East: ')
 hallway_15 = Room("Long Corridor", 'hallway_17', 'hallway_14', None, None,  # Decided to Remove 'hallway_16' unnecessary
-                  {None}, None, "This Long Hallway leads to: "
-                  "North: Long Corridor "
-                  "South: Long Corridor "
-                  "East: "
-                  "West: ")
+                  None, None, 'This Long Hallway leads to: '
+                              'North: Long Corridor '
+                              'South: Long Corridor '
+                              'East: '
+                              'West: ')
 hallway_17 = Room("Long Corridor", None, 'hallway_15', None, None,
-                  {None}, None, "This Long Hallway leads to: "
-                  "North: "
-                  "South: Long Corridor "
-                  "East: "
-                  "West: ")
+                  None, None, 'This Long Hallway leads to: '
+                              'North: '
+                              'South: Long Corridor '
+                              'East: '
+                              'West: ')
 hallway_18 = Room("Long Corridor", None, None, 'hallway_13', 'hallway_20',  # Decided to remove 'hallway_19' unnecessary
-                  {None}, None, "This Long Hallway leads to: "
-                  "North: "
-                  "South: "
-                  "East: Long Corridor "
-                  "West: Long Corridor ")
+                  None, None, 'This Long Hallway leads to: '
+                              'North: '
+                              'South: '
+                              'East: Long Corridor '
+                              'West: Long Corridor ')
 hallway_20 = Room("Long Corridor", None, None, 'hallway_18', 'hallway_21',
-                  {None}, None, "This Long Hallway leads to: "
-                  "North: "
-                  "East: Long Corridor "
-                  "West: Long Corridor ")
-hallway_21 = Room("Long Corridor", None, None, 'hallway_20', 'hallway_23', {None}, None, "This Long Hallway leads to: "
-                  "North: "
-                  "South: "
-                  "East: Long Corridor "
-                  "West: Long Corridor ")
-hallway_23 = Room("Long Corridor", None, None, None, 'hallway_23', {None}, None, "This Long Hallway leads to: "
-                  "North: "
-                  "East: "
-                  "West: Long Corridor ")
-hallway_24 = Room("Long Corridor", 'hallway_5', None, None, 'hallway_12', {None}, None, "This Long Hallway Leads to: "
-                  "North: Long Corridor "
-                  "West: Long Corridor ")
-the_entrance = Room("The Entrance", None, 'empty_room', None, None, {sword}, None,
-                    "You enter a room through a ladder, from where the top leads to an abandoned subway tunnel... "
-                    "But your mission is to investigate where this place leads and eliminate any threats. To the south "
-                    "there is a hallway leading to a room.")
-empty_room = Room("An Empty Room", 'the_entrance', 'leaking_room', None, None, {None}, None,
-                  "There is nothing in here, but mops and brooms. The smell of sewer water makes you feel sick.")
+                  None, None, 'This Long Hallway leads to: '
+                              'North: '
+                              'East: Long Corridor '
+                              'West: Long Corridor ')
+hallway_21 = Room("Long Corridor", None, None, 'hallway_20', 'hallway_23', None, None, 'This Long Hallway leads to: '
+                                                                                       'North: '
+                                                                                       'South: '
+                                                                                       'East: Long Corridor '
+                                                                                       'West: Long Corridor ')
+hallway_23 = Room("Long Corridor", None, None, None, 'hallway_23', None, None, 'This Long Hallway leads to: '
+                                                                               'North: '
+                                                                               'East: '
+                                                                               'West: Long Corridor ')
+hallway_24 = Room("Long Corridor", 'hallway_5', None, None, 'hallway_12', None, None, 'This Long Hallway Leads to: '
+                                                                                      'North: Long Corridor '
+                                                                                      'West: Long Corridor ')
+the_entrance = Room("The Entrance", None, 'empty_room', None, None, Item[sword], None,
+                    'You enter a room through a ladder, from where the top leads to an abandoned subway tunnel... '
+                    'But your mission is to investigate where this place leads and eliminate any threats. To the south '
+                    'there is a hallway leading to a room.')
+empty_room = Room("An Empty Room", 'the_entrance', 'leaking_room', None, None, None, None,
+                  'There is nothing in here, but mops and brooms. The smell of sewer water makes you feel sick.')
 leaking_room = Room("The Leaking Room", 'empty_room', None, None, 'wall_opening',
                     {None}, None,
-                    "You find yourself in another room, there's a cold breeze from the giant hole in the west wall."
-                    "The other walls have water dripping from the sewer pipes.")
-wall_opening = Room("Broken Wall Opening", None, None, 'leaking_room', 'hallway_1', {None}, None,
-                    "You are inside an opening of a brick wall. The inside of the wall is filled with pipes. "
-                    "It's a tight squeeze but at least the rats aren't crawling up your legs.")
-broom_closet = Room("The Broom Closet", None, None, None, 'hallway_2', {None}, None,
-                    "There's nothing in here, but mops and brooms. There is a smell of chemicals coming from "
-                    "the sewer pipes.")
-alchemy_lab = Room("Secret Alchemy Lab", None, 'library', None, None, {health_potion}, None,
-                   "Many different types of ingredients surround you on shelves. "
-                   "'ThErE iS nO wAy OuT', says a tall mysterious figure.")
-guard_room = Room("The Guard's Room", None, None, None, None, {sword}, troll,
-                  "There are various weapons scattered across the floor. The skeletons of dead guards are piled in the "
-                  "corner. It seems that you are not alone in this room.")
+                    'You find yourself in another room, there\'s a cold breeze from the giant hole in the west wall.'
+                    'The other walls have water dripping from the sewer pipes.')
+wall_opening = Room("Broken Wall Opening", None, None, 'leaking_room', 'hallway_1', None, None,
+                    'You are inside an opening of a brick wall. The inside of the wall is filled with pipes. '
+                    'It\'s a tight squeeze but at least the rats aren\'t crawling up your legs.')
+broom_closet = Room("The Broom Closet", None, None, None, 'hallway_2', None, None,
+                    'There\'s nothing in here, but mops and brooms. There is a smell of chemicals coming from '
+                    'the sewer pipes.')
+alchemy_lab = Room("Secret Alchemy Lab", None, 'library', None, None, Item[health_potion], None,
+                   'Many different types of ingredients surround you on shelves. '
+                   '\'ThErE iS nO wAy OuT\', says a tall mysterious figure.')
+guard_room = Room("The Guard's Room", None, None, None, None, Item[sword], troll,
+                  'There are various weapons scattered across the floor. The skeletons of dead guards are piled in the '
+                  'corner. It seems that you are not alone in this room.')
 
 
 inventory = {}
 current_node = the_entrance
-current_item = the_entrance.items
-inv1 = current_item
 directions = ['north', 'south', 'east', 'west']
 short_directions = ['n', 's', 'e', 'w']
 
@@ -302,7 +288,8 @@ while True:
             print("")
     elif command == 'i':
         try:
-            print(inventory)
+            for item in items.values():
+                print('\t'.join([str(x) for x in [item.name]]))
         except KeyError:
             print("There's Nothing in your inventory.")
             print()
@@ -315,8 +302,8 @@ while True:
             print("What do you want to take?")
             input()
             if input == current_node.items:
-                current_item.take(command)
-                inv1.append(current_item)
+                items[item.name] = current_item
+                inventory.append(current_item)
             # print("What do you want to take?")
         except KeyError:
             print("Nothing here to take.")
