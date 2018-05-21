090f0da0faabcdea3b47fef577983105d76a2803
Rene-Angel <31977772+Rene-Angel@users.noreply.github.com>
2018-05-21 11:48:49 -0700

    5.21.18
    Fixed Take Function and added in more rooms and other fuctions.

--- World Map Class Notes.py
+++ World Map Class Notes.py
@@ -81,11 +81,6 @@ class Character(object):
     def take_damage(self, dmg):
         self.health(dmg)
 
-    def equip(self, item):
-        if isinstance(item, Weapon):
-            print("---------------------------------------------------------------------------------------------")
-            print("New item equipped.")
-
 
 class Room(object):
     def __init__(self, name, north, south, east, west, roominv, character, description):
@@ -101,8 +96,6 @@ class Room(object):
     def move(self, direction):
         global current_node
         current_node = globals()[getattr(self, direction)]
-
-        # def addtoinventory(self, item):
 
 
 sword = Sword()
@@ -122,6 +115,8 @@ goblin = Character("Goblin", 100, 10, None, [dagger], [egg])
 skeleton = Character("Skeleton", 75, 15, None, [sword], [None])
 dragon = Character("Dragon", 1000, 45, None, [None], [None])
 ghost = Character("Ghost", 50, 10, None, [None], [None])
+prisoner_spirit = Character("Prisoner's Spirit", 125, 30, None, [None], [None])
+mimic = Character("Mimic", 300, 25, None, [None], [None])
 
 START = Room("Main Hall - Entrance", None, 'HALL', 'LIBRARY', 'LABORATORY', [sword], troll,
              "You are at the entrance of this dark dungeon. A large dark oak wood door stands at your northern side "
@@ -133,21 +128,46 @@ LIBRARY = Room("Library", None, None, None, 'START', [sword], None,
 LABORATORY = Room("Alchemy Laboratory", None, None, 'Start', None, [None], None,
                   "There are shelves on the walls stacked with various ingredients and potions. The floor is marked "
                   "with an alchemist's circle of transmutation, and there is a small table in the corner of the room.")
-HALL = Room("Main Hall - Hallway", 'START', 'DINING', None, None, [None], None,
-            "The Main Hall of this dungeon continues toward south. Skeletal corpses lay on the floor that have been "
-            "recently scorched. There was a faintly lit lantern on the floor, but by the looks of it you are not "
-            "alone. You hear footsteps.")
+HALL = Room("Main Hall - Hallway", 'START', 'DINING', 'CORRIDOR', None, [None], None,
+            "The Main Hall of this dungeon continues toward a dining area to the south, and another hall to the east. "
+            "Skeletal corpses lay on the floor that have been recently scorched. There was a faintly lit lantern on "
+            "the floor, but by the looks of it you are not alone. You hear footsteps.")
+CORRIDOR = Room("Main Hall - Corridor", 'CLOSET', 'KITCHEN', 'DEAD_END', 'HALL', [None], None,
+                "A dead end is is lead to your east, the main hallway to the west, the kitchen to your south, "
+                "and a closet to your north. There are more corpses on the floor in a trail leading you to the dead "
+                "end. You hear footsteps once more...")
+KITCHEN = Room("The Kitchen", 'CORRIDOR', None, 'LOCKER', 'DINING', [None], None,
+               "The corridor is to the north, dining room to west, and frozen meat locker to the east. A sink filled "
+               "with unwashed pots and pans releases an odor most foul. Many food items though seem fresh but rotten, "
+               "although there is a striking red fruit that stands out the most. Luckily you know not to eat anything "
+               "that's fake or poisoned.")
+LOCKER = Room("Meat Locker", None, None, None, 'KITCHEN', [None], None,
+              "You enter the frozen meat locker from the disgusting kitchen. The meat of many animals haunts your "
+              "mind as you see them mutilated and hanged on hooks, but there are also more bodies and even they are "
+              "hanged here.\nWhy would anybody do this?")
 DINING = Room("Dining Room", 'HALL', 'QUARTERS', 'KITCHEN', None, [None], None,
               "A large round table stands before you with a daggers stabbed through it as if a group of rogues were "
               "plotting here for a kill contract. The dorms are to your south, where you still hear footsteps.")
+QUARTERS = Room("The Quarters", 'DINING', None, 'WEAPONS', 'JAIL', [None], None,
+                "You enter inside what seems like a quarter room for mercenaries. They lay before you in their beds, "
+                "but they were slain in their sleep. Their spirits still haunt the room though. The dining room is to "
+                "the north, the weapons room to your west, and the jail room to the east.")
+JAIL = Room("Jail room", None, None, None, 'QUARTERS', [None], None,
+            "You enter the jail room finding more corpses laying on the floor in each cell. Their lives were taken by "
+            "an unnatural presence, and their spirits haunt the walls and objects crying, 'FREE ME!'. The room leads "
+            "back to the quarters to the west.")
+WEAPONS = Room("Weapons Room", None, None, 'QUARTERS', None, [None], None,
+               "All the weapons before you have been demolished to pieces. Although each and every chest around has "
+               "been looted. There were still some chests that have been untouched. The room leads back to the "
+               "quarters to the east.")
 
 inventory = []
 current_node = START
 directions = ['north', 'south', 'east', 'west']
 short_directions = ['n', 's', 'e', 'w']
-short_look = ['l']
 # interactions = ['take', 'inventory', 'use']
 # short_interactions = ['t', 'i', 'u']
+
 print(" __          __  _                          _ \n"
       " \ \        / / | |                        | |\n"
       "  \ \  /\  / /__| | ___ ___  _ __ ___   ___| |\n"
@@ -162,9 +182,20 @@ print(" __          __  _                          _ \n"
 print(current_node.name)
 print(current_node.description)
 print("---------------------------------------------------------------------------------------------")
+
 while True:
     command = input('>_').lower()
     if command == 'quit':
+        print("---------------------------------------------------------------------------------------------\n"
+              "You quit the game. Goodbye!\n"
+              "---------------------------------------------------------------------------------------------\n"
+              "   _____                         ____                 _ \n"
+              "  / ____|                       / __ \               | |\n"
+              " | |  __  __ _ _ __ ___   ___  | |  | |_   _____ _ __| |\n"
+              " | | |_ |/ _` | '_ ` _ \ / _ \ | |  | \ \ / / _ \ '__| |\n"
+              " | |__| | (_| | | | | | |  __/ | |__| |\ V /  __/ |  |_|\n"
+              "  \_____|\__,_|_| |_| |_|\___|  \____/  \_/ \___|_|  (_)\n"
+              "---------------------------------------------------------------------------------------------")
         quit(0)
     if command in short_directions:
         pos = short_directions.index(command)
@@ -177,18 +208,26 @@ while True:
             print(current_node.description)
             print("---------------------------------------------------------------------------------------------")
         except KeyError:
-            print("You cannot go this way.")
-            print("")
+            print("---------------------------------------------------------------------------------------------")
+            print("You cannot go that way.")
+            print("---------------------------------------------------------------------------------------------")
 
     if command == 'help':
-        print("---------------------------------------------------------------------------------------------")
-        print("t = take")
-        print("i = inventory")
-        print("u = use")
-        print("n, s, e, w = moving in directions 'North', 'South', 'East', 'West'")
-        print("---------------------------------------------------------------------------------------------")
+        print("---------------------------------------------------------------------------------------------\n"
+              "Controls and Descriptions:"
+              "t = take (an item)\n"
+              "equip = equip a weapon (from your inventory)\n"
+              "i = inventory (view items in your inventory)\n"
+              "equipped = weapons equipped (weapons only)\n"
+              "u = use (an item)\n"
+              "l = look (for items in area)\n"
+              "n, s, e, w = 'North', 'South', 'East', 'West' (for moving in directions + typing in the full word works "
+              "too)\n "
+              "---------------------------------------------------------------------------------------------\n"
+              "To review these controls again type in 'help'\n"
+              "---------------------------------------------------------------------------------------------")
 
-    if command == 'w':
+    if command == 'equipped':
         print("---------------------------------------------------------------------------------------------")
         if len(player.weapon) > 0:
             print("You have the following weapons equipped:")
@@ -212,13 +251,13 @@ while True:
 
     if command == 't':
         print("---------------------------------------------------------------------------------------------")
-        print("What do you want to take?")
+        print("What do you want to take? (Type in the item name with a capital.)")
         print("---------------------------------------------------------------------------------------------")
-        item_requested = input(">_ ")
+        item_requested = input(">_")
         found = False
         for item in current_node.roominv:
             if item.name == item_requested:
-                player.inv.append(current_node.roominv)
+                player.inv.append(item)
                 print("---------------------------------------------------------------------------------------------")
                 print("You take %s." % item_requested)
                 print("You now have %s in your inventory." % item.name)
@@ -245,12 +284,12 @@ while True:
     #         else:
     #             print("There's nothing there to take.")
 
-    if command in short_look:
+    if command in 'l':
         print("---------------------------------------------------------------------------------------------")
         if len(current_node.roominv) > 0:
             print("Item(s) in your area:")
-            for Item in current_node.roominv:
-                print(Item.name)
+            for item in current_node.roominv:
+                print(item.name)
             print("---------------------------------------------------------------------------------------------")
         elif len(current_node.roominv) < 0:
             print("There are no Items in your area.")
